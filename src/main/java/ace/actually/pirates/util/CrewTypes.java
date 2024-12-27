ba4ed225548733f9202df37c9d9c4420986f2f94
James Green <jgreenop22@gmail.com>
2024-12-27 12:12:23 -0800

    rename interface

--- src/main/java/ace/actually/pirates/util/CrewTypes.java
+++ src/main/java/ace/actually/pirates/util/CrewTypes.java
@@ -2,6 +2,6 @@ package ace.actually.pirates.util;
 
 import net.minecraft.state.property.EnumProperty;
 
-public interface ModProperties {
+public interface CrewTypes {
     EnumProperty<CrewSpawnType> CREW_SPAWN_TYPE = EnumProperty.of("crew_spawn_type", CrewSpawnType.class);
 }
